688920c4e85f11a8bfa160f3a2b473205c9c1560
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-01 23:04:09 +0300

    Prevent shell expand

--- scripts/data/get_paikannimet.sh
+++ scripts/data/get_paikannimet.sh
@@ -3,5 +3,5 @@
 mkdir -p "data/paikannimet"
 pushd "data/paikannimet"
 wget http://kartat.kapsi.fi/files/nimisto/paikannimet_kaikki/etrs89/gml/paikannimet_2020_01.zip
-unzip -j -o *.zip
+unzip -j -o "*.zip"
 popd
