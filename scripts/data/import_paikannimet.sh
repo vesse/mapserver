bf7fcb959f78aa4d6a3470a903e51c9767da0491
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-01 23:05:26 +0300

    Add new param to imports

--- scripts/data/import_paikannimet.sh
+++ scripts/data/import_paikannimet.sh
@@ -3,6 +3,6 @@
 source scripts/helpers.sh
 
 echo "Import paikannimet"
-importData "data/paikannimet" "" "\"kieliKoodi\"='fin'" "paikannimi.xml"
+importData "data/paikannimet" "" "\"kieliKoodi\"='fin'" 0 "paikannimi.xml"
 
 execSql 'CREATE INDEX paikannimi_mittakaavarelevanssikoodi_idx ON paikannimi(mittakaavarelevanssikoodi)'
