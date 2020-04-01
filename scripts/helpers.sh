db5eb7f2484da5eae771775cf4132d044fba60bb
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-01 23:03:25 +0300

    Prepare for maastotietokanta import, no file names to tables there

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -38,12 +38,15 @@ function rsyncKapsi {
 # param $1 Source folder
 # param $2 Table base name
 # param $3 Filter
+# param $4 1 table base name only, 0 to append file name to base name
 # param $4 List of files to import
 function importData {
   folder=${1}
   table_basename=${2}
   filter=${3}
+  basename_only=${4}
 
+  shift
   shift
   shift
   shift
@@ -51,18 +54,31 @@ function importData {
 
   pushd "${folder}" > /dev/null
 
+  # Add underscore if base name is not empty and file name will be appended
   if [ ! -z "${table_basename}" ]; then
-    table_basename="${table_basename}_"
+    if [ $basename_only = 0 ]; then
+      table_basename="${table_basename}_"
+    fi
+  elif [ $basename_only = 1 ]; then
+    (>&2 echo "ERROR: Cannot use basename only without basename")
+    exit
   fi
 
   yearpattern='(.*)_[0-9]{4}_(.*)'
 
   for f in ${files[@]}; do
     if [ -f ${f} ]; then
-      table_name="${table_basename}${f%.*}"
+      # Table name is basename + file name without exentension
+      if [ $basename_only = 0 ]; then
+        table_name="${table_basename}${f%.*}"
+      else
+        table_name="${table_basename}"
+      fi
+      # Bash 4 non-posix lowercase
       table_name="${table_name,,}"
 
       # Remove _year_ which is at least in municipality limits
+      # TODO: Seems there no such thing nowadays?
       while [[ $table_name =~ $yearpattern ]]; do
         table_name=${BASH_REMATCH[1]}_${BASH_REMATCH[2]}
       done
