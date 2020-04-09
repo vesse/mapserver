27208b3318078957713373b18560048452d1d684
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-09 18:27:23 +0300

    Download Maastotietokanta from MML website

--- scripts/data/get_maastotietokanta.sh
+++ scripts/data/get_maastotietokanta.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-#source scripts/helpers.sh
+source config
 
-# TODO: Kapsi rsync is not responding
-#rsyncKapsi "data/maastotietokanta" "maastotietokanta/kaikki/etrs89/gml/P5/P54"
+initial_uri="https://tiedostopalvelu.maanmittauslaitos.fi/tp/feed/mtp/maastotietokanta/kaikki?api_key=${MML_API_KEY}&format=application/x-shapefile"
+tmp_file="./maastotietokanta.xml"
 
 mkdir -p "data/maastotietokanta"
-pushd "data/maastotietokanta" >/dev/null
-echo "Start mirroring from kartat.kapsi.fi in quiet mode. This will take time."
-wget -m -np http://kartat.kapsi.fi/files/maastotietokanta/kaikki/etrs89/shp/
-find . -name "*.html" -exec rm {} \;
-echo "Done mirroring"
-popd >/dev/null
+pushd "data/maastotietokanta"
+
+url=$initial_uri
+while [ ! -z "${url}" ]; do
+  curl -o ${tmp_file} "${url}"
+
+  # Download all files from the listing
+  # xmlstarlet is picky with xmlns set in source document
+  xml sel \
+    -N x="http://www.w3.org/2005/Atom" \
+    -t -v "//x:feed/x:entry/x:link/@href" ${tmp_file} \
+    | perl -MHTML::Entities -pe 'decode_entities($_);' \
+    | wget --content-disposition -i -
+
+  # See if there is more data to download - the XML contains maximum of 1000 entries
+  url=`xml sel -N x="http://www.w3.org/2005/Atom" -t -v "//x:feed/x:link[@rel='next']/@href" ${tmp_file} | perl -MHTML::Entities -pe 'decode_entities($_);'`
+done
+
+popd
