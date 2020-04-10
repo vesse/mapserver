1d5148e78a6298402f0113c01a8f63523a6efc3a
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-10 19:54:23 +0300

    Import Tampere and start checking what we got and what is needed

--- scripts/data/import_maastotietokanta.sh
+++ scripts/data/import_maastotietokanta.sh
@@ -7,6 +7,7 @@
 source scripts/helpers.sh
 
 pushd "data/maastotietokanta"
+mkdir -p "tmp"
 
 # https://stackoverflow.com/a/17841619/3349511
 function join_by { local IFS="$1"; shift; echo "$*"; }
@@ -55,9 +56,34 @@ AREAS=(
   42260
   42261
   42262
+
+  72200 # Luonnonsuojelualue
+  72201 # Luonnonpuisto
+  72202 # Kansallispuisto
+  72800 # Retkeilyalue
+  72500 # Suojametsä
+
+  40200 # Taajaan asuttu alue
+)
+
+LINES=(
+  84113 # Kunnan raja
+  84115 # Maakunnan raja
+
+  36311 # Virtavesi <2m
+  36312 # Virtavesi 2-5m
+  38200 # Koski
+
+  45111 # Pistolaituri
+  45112 # Pistolaituri
+
+  12313 # Polku
+  12314 # Kävely- ja pyörätie
+  12316 # Ajopolku
 )
 
 _areas=$(join_by , "${AREAS[@]}")
+_lines=$(join_by , "${LINES[@]}")
 
 
 for f in *.zip; do
@@ -65,7 +91,7 @@ for f in *.zip; do
   # TODO: Should different geometry types go to different tables?
   #       Or maybe _p _v etc. to their own? Probably latter as the import fails
   #       of putting polygons to point tables.
-  if [[ $f =~ M4224R ]]; then
+  if [[ $f =~ M4 ]]; then
     unzip $f -d ./tmp >/dev/null
     pushd tmp >/dev/null
     # Maastotietokanta zip files contain multiple files. Files with certain suffix
@@ -79,9 +105,9 @@ for f in *.zip; do
       importData "." "maastotietokanta_p" "LUOKKA IN (${_areas})" 1 "*_p.shp"
     fi
 
-    # if ls *_v.shp >/dev/null 2>&1; then
-    #   importData "." "maastotietokanta_v" "LUOKKA IN (${_lines})" 1 "*_v.shp"
-    # fi
+    if ls *_v.shp >/dev/null 2>&1; then
+      importData "." "maastotietokanta_v" "LUOKKA IN (${_lines})" 1 "*_v.shp"
+    fi
 
     # if ls *_s.shp >/dev/null 2>&1; then
     #   importData "." "maastotietokanta_s" "" 1 "*_s.shp"
@@ -92,8 +118,11 @@ for f in *.zip; do
     # fi
 
     popd
-    #rm tmp/*
+    rm tmp/*
   fi
 done
+
+execSql 'CREATE INDEX maastotietokanta_p_luokka_idx ON maastotietokanta_p(luokka)'
+execSql 'CREATE INDEX maastotietokanta_v_luokka_idx ON maastotietokanta_v(luokka)'
 
 popd
