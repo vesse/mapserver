d7d8293d3fdd68d67ad31cb8f56bc4781793e971
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2020-04-15 21:32:54 +0300

    Name downloads

--- scripts/data/get_paikannimet.sh
+++ scripts/data/get_paikannimet.sh
@@ -2,6 +2,6 @@
 
 mkdir -p "data/paikannimet"
 pushd "data/paikannimet"
-wget http://kartat.kapsi.fi/files/nimisto/paikannimet_kaikki/etrs89/gml/paikannimet_2020_01.zip
+wget -O paikannimet.zip http://kartat.kapsi.fi/files/nimisto/paikannimet_kaikki/etrs89/gml/paikannimet_2020_01.zip
 unzip -j -o "*.zip"
 popd
--- scripts/data/get_yleiskartta.sh
+++ scripts/data/get_yleiskartta.sh
@@ -2,12 +2,12 @@
 
 mkdir -p "data/yleiskartta_4500k"
 pushd "data/yleiskartta_4500k"
-wget http://kartat.kapsi.fi/files/yleiskartta_4500k/kaikki/etrs89/shape/4_5_milj_shape_etrs-tm35fin.zip
+wget -O 4_5_milj_shape_etrs-tm35fin.zip http://kartat.kapsi.fi/files/yleiskartta_4500k/kaikki/etrs89/shape/4_5_milj_shape_etrs-tm35fin.zip
 unzip -j -o "*.zip"
 popd
 
 mkdir -p "data/yleiskartta_1000k"
 pushd "data/yleiskartta_1000k"
-wget http://kartat.kapsi.fi/files/yleiskartta_1000k/kaikki/etrs89/shape/1_milj_Shape_etrs_shape.zip
+wget -O 1_milj_Shape_etrs_shape.zip http://kartat.kapsi.fi/files/yleiskartta_1000k/kaikki/etrs89/shape/1_milj_Shape_etrs_shape.zip
 unzip -j -o "*.zip"
 popd
