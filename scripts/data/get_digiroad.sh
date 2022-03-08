22e318747effb5065aee1893aef614880bedc2d4
Vesa Poikajärvi <vesa.poikajarvi@iki.fi>
2022-03-08 20:35:42 +0200

    Update get_digiroad.sh

--- scripts/data/get_digiroad.sh
+++ scripts/data/get_digiroad.sh
@@ -2,6 +2,6 @@
 
 mkdir -p "data/digiroad"
 pushd "data/digiroad"
-wget -O KokoSuomi_DIGIROAD_R_EUREF-FIN.zip https://aineistot.vayla.fi/digiroad/latest/KokoSuomi_DIGIROAD_R_EUREF-FIN.zip
+wget -O KokoSuomi_DIGIROAD_R_EUREF-FIN.zip "https://api.vaylapilvi.fi/ava/hakemisto/ava/Tiest%C3%B6tiedot/Digiroad/Digiroad-irrotusaineistot/latest/KokoSuomi_DIGIROAD_R_EUREF-FIN.zip"
 unzip -o "*.zip"
 popd
